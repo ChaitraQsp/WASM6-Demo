942f4359615c1ca0e21aea9f04cf7512572c1fc4
ChaitraQsp <chaitraqspvirtual@gmail.com>
2022-11-19 11:44:20 +0530

    Commit 1 - DemoTest

---
+++ src/test/java/demo/DemoTest.java
@@ -0,0 +1,16 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	
+   
+	@Test
+	public void demo()
+	{
+		
+		System.out.println("HI This is wasm 6 Franmework!!!");
+		
+	}
+
+}
